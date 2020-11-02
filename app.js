18336ff99ea32a37c25d1a8312c841f2558e21ef
eriveros <eriveros298@gmail.com>
2020-11-02 20:39:57 -0300

    solucion del ejercicio

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 // PASO 1: Crear una constante llamada express y requerir el modulo express
-
+const express = require('express')
 // PASO 2: Ejecución de Express
 // Recorda que express es una funcion... hay que invocarla y guardalo en una constante llamada app
-
+const app = express()
 // PASO 3: Informacion que vamos a usar
 // Crea una variable (por ej. heroes) y asignale como valor el contenido del archivo "heroes.json"
 // Tip: podes usar el require como si fuera un modulo ;)
-
+const heroes=require('./heroes.json') //si el archivo es .json te deja 
+console.log(heroes)
 // PASO 4: Configurando el servidor
 // Crea una constante llamada PORT y asignale como valor el numero tres mil
 // ...
-
+const  PORT=8081
 // Reemplaza los guiones por la constante para que utilice ese numero de puerto
 // Sabes por que lo definimos como constante y no como variable ? 
-app.listen(____, () => console.log(`Server running in ${____} port`));
-
+app.listen(PORT, () => console.log(`Server running in ${PORT} port`));
+const home_message="Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Héroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!"
 // CONSIGNA 1
 // Ruta Raíz / Home
-app.get('/', _que_debemos_hacer_aqui_para_que_funcione_?_);
+app.get('/', function(req,res){
+  res.send(JSON.stringify(home_message))
+});
 
 // CONSIGNA 2
 // Ruta /heroes
 // Consigna: enviar todo el array
-app.get(_nombre_de_ruta_, (_que_parametros_necesita_?_) => {
-  res.send(_donde_estan_los_datos_que_voy_a_enviar_?_);
+app.get("/heroes", function (req,res) {
+  res.json(heroes)
 });
 
 // CONSIGNA 3
 // Aqui creas la ruta para devolver la pagina de los creditos
 // Podes resolverlo en base a los ejemplos anteriores.
-
+app.get("/creditos",function(req,res){
+  res.json({
+    name:"Ezequiel",
+    frase:"The road to enlightenment is long and difficult, and you should try not to forget snacks and magazines."
+  })
+})
 
 // Ruta... ¿Pára qué sirve esto?
 app.get('*', (req, res) => {
